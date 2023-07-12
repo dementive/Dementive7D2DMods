4d9f8779e299dfd320097f52a6cc990fa9ed8585
dementive <nathanvandrei@yahoo.com>
2023-07-12 01:00:48 -0400

    Quests expanded C#

---
+++ QuestsExpanded/src/Harmony/init.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+public class QuestsExpanded : IModApi
+{
+    public void InitMod(Mod _modInstance)
+    {
+        var harmony = new HarmonyLib.Harmony(GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ QuestsExpanded/src/MinEvents/MinEventActionPumpQuestSDX.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+public class MinEventActionPumpQuestSDX : MinEventActionRemoveBuff
+{
+    // This loops through all the targets, refreshing the quest. 
+    //  <triggered_effect trigger="onSelfBuffStart" action="PumpQuestSDX, SCore" target="self"  />
+    public override void Execute(MinEventParams _params)
+    {
+        for (var j = 0; j < targets.Count; j++)
+        {
+            var entityPlayer = targets[j] as EntityPlayerLocal;
+            if (entityPlayer != null)
+                for (var k = 0; k < entityPlayer.QuestJournal.quests.Count; k++)
+                    for (var l = 0; l < entityPlayer.QuestJournal.quests[k].Objectives.Count; l++)
+                        entityPlayer.QuestJournal.quests[k].Objectives[l].Refresh();
+        }
+    }
+}
