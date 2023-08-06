09309f2c2ea0277350845c55fd5f7ce00ec10a60
dementive <nathanvandrei@yahoo.com>
2023-08-06 14:53:08 -0400

    Entity groups script

---
+++ Scripts/make_entitygroups.py
@@ -0,0 +1,214 @@
+from entity_groups import Groups
+
+normal_zombies = """
+    zombieGuppyThorton
+    zombieGuppyStripper
+    zombieGuppySeth
+    zombieGuppySecurityGuard
+    zombieGuppyRoland
+    zombieGuppyPoliceRalph
+    zombieGuppyPete
+    zombieGuppyOldManZombie
+    zombieGuppyCheerleader
+    zombieGuppyNurse
+    zombieGuppyLucy
+    zombieGuppyKennethClown
+    zombieGuppyHungryScott
+    zombieGuppyHungryJeff
+    zombieGuppyHungryGeorge
+    zombieGuppyGermanCosplay
+    zombieGuppyClot
+    zombieGuppyCarmela
+    zombieGuppyBob
+    zombieGuppyBilly
+    zombieGuppyBelle
+    zombieGuppyBeatCop
+    zombieGuppyAlma
+    none, 0.0
+"""
+
+wasteland_zombies = """
+    zombieGuppyThorton
+    zombieGuppyStripper
+    zombieGuppySeth
+    zombieGuppySecurityGuard
+    zombieGuppyRoland
+    zombieGuppyPoliceRalph
+    zombieGuppyPete
+    zombieGuppyOldManZombie
+    zombieGuppyCheerleader
+    zombieGuppyNurse
+    zombieGuppyLucy
+    zombieGuppyKennethClown
+    zombieGuppyHungryScott
+    zombieGuppyHungryJeff
+    zombieGuppyHungryGeorge
+    zombieGuppyGermanCosplay
+    zombieGuppyClot
+    zombieGuppyCarmela
+    zombieGuppyBob
+    zombieGuppyBiomechanicalWight, 0.05
+    zombieGuppyBilly
+    zombieGuppyBelle
+    zombieGuppyBeatCop
+    zombieGuppyAlma
+    zombieGuppyBabyBoomer
+    none, 0.0
+"""
+
+feral_zombies = """
+    zombieGuppyBiomechanicalWight, 0.05
+    zombieGuppyBabyBoomer
+    none, 0.0
+"""
+
+boss_zombies = """
+    zombieGuppyRoland
+    none, 0.0
+"""
+
+buissness_man_zombies = """
+    zombieGuppyRoland
+    none, 0.0
+"""
+
+fat_cop_zombies = """
+    zombieGuppyBeatCop
+    zombieGuppyPoliceRalph
+    zombieGuppySecurityGuard
+    none, 0.0
+"""
+
+farm_zombies = """
+    zombieGuppyOldManZombie
+    zombieGuppyHungryScott
+    zombieGuppyHungryJeff
+    zombieGuppyHungryGeorge
+    none, 0.0
+"""
+
+soldier_zombies = """
+    zombieGuppyGermanCosplay
+    none, 0.0
+"""
+
+hazmat_zombies = """
+    zombieGuppyClot
+    none, 0.0
+"""
+
+utility_worker_zombies = """
+    zombieGuppyThorton
+    zombieGuppyPete
+    none, 0.0
+"""
+
+ghost_town_zombies = """
+    zombieGuppyAlma
+    zombieGuppySeth
+    zombieGuppyLucy
+    zombieGuppyBob
+    zombieGuppyBilly
+    none, 0.0
+"""
+
+hospital_zombies = """
+    zombieGuppyNurse
+    zombieGuppyBelle
+    none, 0.0
+"""
+
+lab_worker_zombies = """
+    zombieGuppyNurse
+    zombieGuppySecurityGuard
+    none, 0.0
+"""
+
+biker_zombies = """
+    zombieGuppyStripper
+    zombieGuppyCheerleader
+    none, 0.0
+"""
+
+night_club_zombies = """
+    zombieGuppyStripper
+    zombieGuppyCheerleader
+    zombieGuppyCarmela
+    zombieGuppyBelle
+    none, 0.0
+"""
+
+special_zombies = """
+    zombieGuppyBiomechanicalWight, 0.05
+    zombieGuppyKennethClown
+    none, 0.0
+"""
+
+wandering_zombies = """
+    zombieGuppyThorton
+    zombieGuppyBelle
+    zombieGuppyAlma
+    zombieGuppyBob
+    zombieGuppyBilly
+    zombieGuppyHungryJeff
+    zombieGuppyHungryGeorge
+    none, 0.0
+"""
+
+sleeper_horder_zombies = """
+    zombieGuppyThorton
+    zombieGuppyStripper
+    zombieGuppySeth
+    zombieGuppySecurityGuard
+    zombieGuppyPoliceRalph
+    zombieGuppyPete
+    zombieGuppyOldManZombie
+    zombieGuppyCheerleader
+    zombieGuppyNurse
+    zombieGuppyLucy
+    zombieGuppyHungryJeff
+    zombieGuppyHungryGeorge
+    zombieGuppyGermanCosplay
+    zombieGuppyClot
+    zombieGuppyCarmela
+    zombieGuppyBilly
+    zombieGuppyBelle
+    zombieGuppyAlma
+    none, 0.0
+"""
+
+
+def write_group(group_list, group_zombies):
+    with open("output_groups.xml", "w") as file:
+        file.write("<configs>\n")
+    with open("output_groups.xml", "a") as file:
+        for i in group_list:
+            file.write(
+                f'<csv xpath="/entitygroups/entitygroup[@name=\'{i}\']/text()" delim="\\n" op="add" >'
+            )
+            file.write(group_zombies)
+            file.write(f"</csv>\n")
+    with open("output_groups.xml", "a") as file:
+        file.write("</configs>")
+
+
+if __name__ == "__main__":
+    groups = Groups()
+    write_group(groups.normal_group, normal_zombies)
+    write_group(groups.wasteland_group, wasteland_zombies)
+    write_group(groups.feral_group, feral_zombies)
+    write_group(groups.boss_group, boss_zombies)
+    write_group(groups.buissness_man_group, buissness_man_zombies)
+    write_group(groups.fat_cop_group, fat_cop_zombies)
+    write_group(groups.farm_group, farm_zombies)
+    write_group(groups.soldier_group, soldier_zombies)
+    write_group(groups.hazmat_group, hazmat_zombies)
+    write_group(groups.utility_worker_group, utility_worker_zombies)
+    write_group(groups.ghost_town_group, ghost_town_zombies)
+    write_group(groups.hospital_group, hospital_zombies)
+    write_group(groups.lab_worker_group, lab_worker_zombies)
+    write_group(groups.biker_group, biker_zombies)
+    write_group(groups.night_club_group, night_club_zombies)
+    write_group(groups.special_group, special_zombies)
+    write_group(groups.wandering_group, wandering_zombies)
+    write_group(groups.sleeper_horder_group, sleeper_horder_zombies)
